22b00e2e332fb48914be23e37b5883abe13a70ba
Xeo872 <156670334+Xeo872@users.noreply.github.com>
2026-05-20 11:05:11 +0530

    remove drm

--- launcher/minecraft/auth/AccountList.cpp
+++ launcher/minecraft/auth/AccountList.cpp
@@ -591,11 +591,7 @@ void AccountList::setListFilePath(QString path, bool autosave)
 
 bool AccountList::anyAccountIsValid()
 {
-    for (auto account : m_accounts) {
-        if (account->ownsMinecraft()) {
-            return true;
-        }
-    }
+    
     return true;
 }
 
